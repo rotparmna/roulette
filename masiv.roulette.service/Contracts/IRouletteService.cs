646432d6bdd2266ab0d9ed1caf25895ac34b6d10
Michael Navarro Ariza <rotpar@gmail.com>
2021-02-16 21:37:18 -0500

    Se crea endpoint para agregar ruleta
    Se crea endpoint para agregar ruleta

---
+++ masiv.roulette.service/Contracts/IRouletteService.cs
@@ -0,0 +1,12 @@
+﻿using Masiv.Roulette.API.Domain.Dtos;
+using System.Collections.Generic;
+
+namespace Masiv.Roulette.API.Contracts
+{
+    public interface IRouletteService
+    {
+        RouletteAddDto Add();
+
+        List<RouletteAddDto> GetAll();
+    }
+}
